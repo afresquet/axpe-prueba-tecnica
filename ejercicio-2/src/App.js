7bce03cd1e9a5b595c0ae7c3de695504a482fc41
afresquet <alvarofresquet@gmail.com>
2021-02-16 19:41:03 +0100

    map setup

--- ejercicio-2/src/App.js
+++ ejercicio-2/src/App.js
@@ -1,5 +1,37 @@
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import "./App.css";
+
+// Coordinates of Barcelona
+const center = {
+	lat: 41.3851,
+	lng: 2.1734,
+};
+
 const App = () => {
-	return <div className="App">AXPE Prueba Técnica - Alvaro Fresquet</div>;
+	const { isLoaded, loadError } = useJsApiLoader({
+		googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+	});
+
+	if (!isLoaded) {
+		return <span>loading...</span>;
+	}
+
+	if (loadError) {
+		return (
+			<div>
+				<span>Map could not be loaded.</span>
+				<pre>{loadError.message}</pre>
+			</div>
+		);
+	}
+
+	return (
+		<GoogleMap
+			mapContainerClassName="map-container"
+			center={center}
+			zoom={12}
+		></GoogleMap>
+	);
 };
 
 export default App;
